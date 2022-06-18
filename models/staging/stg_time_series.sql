43d616a66e7e295dfb489b96a03a8a5358d9ee4d
Allen Wang <allenwangs@gmail.com>
2022-06-18 09:51:56 +0000

    add fct_time_series.yml

--- models/staging/stg_time_series.sql
+++ models/staging/stg_time_series.sql
@@ -1,2 +1,28 @@
-select *
-from {{ source('battery_archive', 'time_series') }}
+with source as (
+
+    select * from {{ source('battery_archive', 'time_series') }}
+
+),
+
+cast_datatype as (
+
+    select
+        file_name,
+        file_row_number,
+        date_time::timestamp,
+        test_time::float,
+        cycle_index::int,
+        cell_current::float,
+        voltage::float,
+        charge_capacity::float,
+        discharge_capacity::float,
+        charge_energy::float,
+        discharge_energy::float,
+        environment_temperature::float,
+        cell_temperature::float
+
+    from source
+
+)
+
+select * from cast_datatype

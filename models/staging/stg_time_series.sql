f5c7b768c7ed3ba0c4ef5bc84c3d4b7a8df71f6b
Allen Wang <allenwangs@gmail.com>
2022-06-18 10:15:10 +0000

    correct column name

--- models/staging/stg_time_series.sql
+++ models/staging/stg_time_series.sql
@@ -9,17 +9,17 @@ cast_datatype as (
     select
         file_name,
         file_row_number,
-        date_time::timestamp,
-        test_time::float,
-        cycle_index::int,
-        cell_current::float,
-        voltage::float,
-        charge_capacity::float,
-        discharge_capacity::float,
-        charge_energy::float,
-        discharge_energy::float,
-        environment_temperature::float,
-        cell_temperature::float
+        date_time::timestamp as date_time,
+        test_time::float as test_time,
+        cycle_index::int as cycle_index,
+        cell_current::float as cell_current,
+        voltage::float as voltage,
+        charge_capacity::float as charge_capacity,
+        discharge_capacity::float as discharge_capacity,
+        charge_energy::float as charge_energy,
+        discharge_energy::float as discharge_energy,
+        environment_temperature::float as environment_temperature,
+        cell_temperature::float as cell_temperature
 
     from source
 

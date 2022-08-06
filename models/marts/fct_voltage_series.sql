995410cee4513d78ae57e8f4c0f422ddc6ebe052
Allen Wang <allenwangs@gmail.com>
2022-08-06 02:05:27 +0000

    adjust column

--- models/marts/fct_voltage_series.sql
+++ models/marts/fct_voltage_series.sql
@@ -106,7 +106,6 @@ remove_real_data as (
         file_name,
         cycle_index,
         voltage,
-        voltage_source,
         discharge_capacity_interpolate as discharge_capacity
     from interpolate_discharge_capacity
     where voltage_source = 'interpolate'
--- models/staging/int_voltage_range.sql
+++ models/staging/int_voltage_range.sql
@@ -3,7 +3,7 @@
         materialized='table'
     )
 }}
-
+-- generate row of data starting from 2.000, 2.001, 2.002 to 3.500
 select 
     2.0 + seq4()/1000 as voltage
 from table(generator(rowcount => 1500)) 
